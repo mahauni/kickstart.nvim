90c44c33cd8d93597f1286a0c5501953c72e03d7
Lucas Raoni <lucasraoniha@gmail.com>
2025-08-02 21:58:35 -0300

    Changing tab size

--- init.lua
+++ init.lua
@@ -27,6 +27,12 @@ vim.o.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
+-- Configure tab lenght
+vim.opt.tabstop = 8 -- Always 8 (see :h tabstop)
+vim.opt.softtabstop = 4 -- What you expecting
+vim.opt.shiftwidth = 4 -- What you expecting
+vim.opt.expandtab = true
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
