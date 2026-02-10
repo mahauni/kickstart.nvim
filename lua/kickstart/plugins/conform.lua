3c8758d8b6c9ee9227b1787bfa0807cbc7c3e91d
Lucas Raoni Hideki Antunes <85591742+mahauni@users.noreply.github.com>
2026-02-10 16:36:30 -0300

    Increase timeout_ms from 500 to 2000

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -25,7 +25,7 @@ return {
           return nil
         else
           return {
-            timeout_ms = 500,
+            timeout_ms = 2000,
             lsp_format = 'fallback',
           }
         end
